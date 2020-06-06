b0fc050cf9001197a937caa70c8e9519ee6e1c51
mramoss <moi6ramoss@gmail.com>
2020-06-06 10:00:47 -0400

    Estructura y material angular module

---
+++ src/app/material.module.ts
@@ -0,0 +1,119 @@
+import {CommonModule, DatePipe} from '@angular/common';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatNativeDateModule } from '@angular/material';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatListModule,
+    MatTableModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatSortModule,
+    MatRadioModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatListModule,
+    MatTableModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatSortModule,
+    MatRadioModule,
+    MatAutocompleteModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatNativeDateModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatDividerModule,
+    MatToolbarModule
+  ],
+  providers: [DatePipe]
+})
+export class MaterialModule {
+}
+
---
+++ src/app/modules/doctores/doctores.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DoctoresRoutingModule } from './doctores-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DoctoresRoutingModule
+  ]
+})
+export class DoctoresModule { }
