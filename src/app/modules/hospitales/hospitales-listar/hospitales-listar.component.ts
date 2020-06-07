30e7cd16055815b5957c224f65fb3c2a253d13b4
mramoss <moi6ramoss@gmail.com>
2020-06-07 15:00:07 -0400

    Hospitales rutas y componentes

---
+++ src/app/modules/hospitales/hospitales-listar/hospitales-listar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hospitales-listar',
+  templateUrl: './hospitales-listar.component.html',
+  styleUrls: ['./hospitales-listar.component.scss']
+})
+export class HospitalesListarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
