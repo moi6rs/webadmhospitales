b3cce6c1e8499ec893fddd37d4b541cc45dbeaa7
mramoss <moi6ramoss@gmail.com>
2020-06-07 20:42:43 -0400

    Estructura de menu

---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default/default.component';
+
+
+
+@NgModule({
+  declarations: [DefaultComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LayoutsModule { }
